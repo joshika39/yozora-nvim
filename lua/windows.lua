a5b91e26213c5b0351dc41f81ed075abd02d99a2
Joshua Hegedus <josh.hegedus@outlook.com>
2024-03-15 11:27:04 +0100

    added system spcific plugins

--- lua/windows.lua
+++ lua/windows.lua
@@ -1,1 +1,13 @@
 vim.opt.clipboard:append { 'unnamed' }
+
+local status, packer = pcall(require, 'packer')
+if (not status) then
+  print("packer is not installed")
+  return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+
+end)
