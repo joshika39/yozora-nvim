488fba2531385d53c2e37b3ea1b4a18a9c1268b4
Joshua Hegedus <josh.hegedus@outlook.com>
2024-03-15 16:38:37 +0100

    fixed linux

--- lua/linux.lua
+++ lua/linux.lua
@@ -1,13 +1,2 @@
 vim.opt.clipboard:append { 'unnamedplus' }
-
-local status, packer = pcall(require, 'packer')
-if (not status) then
-  print("packer is not installed")
-  return
-end
-
-vim.cmd [[packadd packer.nvim]]
-
-packer.startup(function(use)
-  use 'f-person/git-blame.nvim'
-end)
+table.insert(Yozora.plugins, { 'f-person/git-blame.nvim' })
