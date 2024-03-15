2dfa772ee7cbb1694c38c52f1cd8ea0bec2ea9ea
Joshua Hegedus <josh.hegedus@outlook.com>
2024-03-15 10:58:26 +0100

    added windows and mac to use the system keyboard

--- init.lua
+++ init.lua
@@ -3,13 +3,18 @@ require('highlights')
 require('maps')
 require('plugins')
 
-local has = function(x) 
+local has = function(x)
   return vim.fn.has(x) == 1
 end
 
 local is_linux = has "unix"
+local is_mac = has "mac"
+local is_windows = has "win32"
 
 if is_linux then
   require("linux")
+elseif is_mac then
+  require("mac")
+elseif is_windows then
+  require("windows")
 end
-
