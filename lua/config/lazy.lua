3012ca482c312977176fd5f9f42d4cc14ad249c4
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2025-01-25 00:06:20 +0100

    fix: needed none-ls

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,6 +28,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
+    { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "plugins" },
   },
   defaults = {
